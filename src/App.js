2315ef3676191b6dfccd04bc4dbce8d69bcec8fa
nv21064-salman-ali <118216333+nv21064-salman-ali@users.noreply.github.com>
2024-04-01 19:07:08 +0000

    updatepush

--- src/App.js
+++ src/App.js
@@ -3,28 +3,25 @@ import BucketUpload from './components/BucketUpload';
 import BucketDownload from './components/BucketDownload';
 import MainLayout from './components/MainLayout';
 
-
-
 function App() {
-   return (
-    <div className="pt-8 flex flex-col items-center justify-center bg-pink-200">
-      <div className="relative isolate overflow-hidden px-6 py-24 text-center shadow-2xl shadow-pink-600 sm:rounded-3xl sm:px-16">
-                    <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-slate-700 sm:text-4xl">
-                        Hello!
-                    </h2>
-                    <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-slate-700">
-                        Welcome to our Project &#128516;
-                    </p>
-              
-
-                </div>
-        <MainLayout>
-        <BucketUpload/>
-        <BucketDownload/>
-        </MainLayout>
-
-    </div>
-  );
+  return (
+   <div className="pt-8 flex flex-col items-center justify-center bg-pink-200">
+     {/* Applying a negative margin-top to move the card up */}
+     <div className="relative isolate overflow-hidden px-6 py-24 text-center shadow-2xl shadow-pink-600 sm:rounded-3xl sm:px-16 -mt-12">
+       <h2 className="mx-auto min-w-1xl text-3xl font-bold tracking-tight text-slate-700 sm:text-4xl">
+           Hello!
+       </h2>
+       <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-slate-700">
+           Welcome to our Project &#128516;
+       </p>
+     </div>
+     <MainLayout>
+       <BucketUpload/>
+       <BucketDownload/>
+     </MainLayout>
+   </div>
+ );
 }
 
 export default App;
+
--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 
-const MainLayout = ({children}) => {
-    return (
-        <div className="bg-pink-200">
-            <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-            <spline-viewer url="https://prod.spline.design/FVZWbQH2B6ndj9UU/scene.splinecode" events-target="global"></spline-viewer>
 
-                <div className="relative isolate overflow-hidden bg-pink-900 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
-                    <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                        Translate your Documents Fast!
-                    </h2>
-                    <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-white">
-                        Start by Uploading a file, wait for it to finish processing, and Download the 
-                        translated version
-                    </p>
-                    {children}
-
-                </div>
-            </div>
+const MainLayout = ({ children }) => {
+  return (
+    <div className="bg-pink-200">
+      <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8 lg:pt-12 lg:mt-12">
+        <div className="spline-wrapper">
+          <spline-viewer url="https://prod.spline.design/FVZWbQH2B6ndj9UU/scene.splinecode" events-target="global"></spline-viewer>
         </div>
-    )
-}
 
-export default MainLayout
+        <div className="relative isolate overflow-hidden bg-pink-900 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+          <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Translate your Documents Fast!
+          </h2>
+          <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-white">
+            Start by Uploading a file, wait for it to finish processing, and Download the translated version
+          </p>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
